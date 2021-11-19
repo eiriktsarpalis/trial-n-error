1e121bce6101ad1241217c1226d733d2a4d40815
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-11-19 16:22:39 +0000

    create minimal repro

---
+++ System.Text.Json.Issue.61737/ClassLibrary1/ClassLibrary.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassLibrary1;
+
+public static class ClassLibrary
+{
+    public static object CreateMetadata() => new System.Text.Json.Serialization.Metadata.JsonObjectInfoValues<int> { ObjectCreator = () => 42 };
+}
--- System.Text.Json.Issue.61737/ConsoleApp1/Program.cs
+++ System.Text.Json.Issue.61737/ConsoleApp1/Program.cs
@@ -1,10 +1,3 @@
 ﻿using ClassLibrary1;
 
-MyModel model1 = new MyModel();
-
-string json1 = Serializer.Serialize(model1);
-string json2 = Serializer.SerializeWithGenerics(model1);
-
-Console.WriteLine(json1);
-Console.WriteLine(json2);
-Console.ReadKey();
+_ = ClassLibrary.CreateMetadata();
